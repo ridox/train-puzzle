4446e7e4854c3aee7ddfb034d514c1f8c4a9a13c
Vova Molin <vmolin@lohika.com>
2018-02-16 23:24:16 +0200

    Fix TrainProcessor

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,20 +1,17 @@
 object App {
 
   def main(args: Array[String]): Unit = {
-    val tp = new TrainProcessor(new Train(-1), new Train(2))
+    val tp = new TrainProcessor(new Train(-1), new Train(1))
     val run = Program()
 
     //TODO think how to remove "run nextCommand" boilerplate
     tp.runProgram {
-      run nextCommand "a" :: Right()
-      run nextCommand GoTo("a")
-      run nextCommand IfStation { Left () }
-      //run nextCommand GoTo("a")
-      /*run nextCommand Left()
+      run nextCommand "1" :: Right()
+      run nextCommand IfStation {GoTo("4")}
+      run nextCommand GoTo("1")
+      run nextCommand "4" :: Right()
       run nextCommand Right()
-      run nextCommand "asd" :: Right()
-      run nextCommand IfStation { Left() }
-      run nextCommand GoTo("asd")*/
+      run nextCommand GoTo("4")
     }
   }
 
--- src/main/scala/TrainProcessor.scala
+++ src/main/scala/TrainProcessor.scala
@@ -6,30 +6,41 @@ class TrainProcessor(val t1: Train, val t2: Train) {
   def runProgram(program: Program) = {
     commands = program.build
 
-    var nextCommandId = 0
+    var nextCommandIdTrain1 = 0
+    var nextCommandIdTrain2 = 0
 
-    while (nextCommandId < commands.length) {
-      nextCommandId = executeCommand(commands(nextCommandId))
-      if (t1.position == 0) println("Station") else println("Tchyh")
+    while (nextCommandIdTrain1 < commands.length && nextCommandIdTrain2 < commands.length && t1.position != t2.position) {
+      nextCommandIdTrain1 = executeCommand(t1, commands(nextCommandIdTrain1))
+      nextCommandIdTrain2 = executeCommand(t2, commands(nextCommandIdTrain2))
+      println("Tchyh")
+      println(s"Train 1 pos:${t1.position}")
+      println(s"Train 2 pos:${t2.position}")
+      if (t1.position == t2.position) println("Boom!!!")
     }
-
   }
 
-  def executeCommand(idAndCommand: (Int, TrainCommand)): Int =
+  def executeCommand(train: Train, idAndCommand: (Int, TrainCommand)): Int =
     idAndCommand match {
       case (commandId, Left()) => {
-        t1.moveLeft
+        train.moveLeft
         commandId + 1
       }
       case (commandId, Right()) => {
-        t1.moveRight
+        train.moveRight
         commandId + 1
       }
-      case (_, GoTo(goToLabel)) =>
-        commands.filter(_._2.label == goToLabel).map(_._1).head
+      case (_, GoTo(goToLabel)) => {
+        commands
+          .filter(t =>
+            t match {
+              case (_, command) => command.label.getOrElse("") == goToLabel
+          })
+          .map(e => e._1)
+          .head
+      }
       case (commandId, IfStation(command)) =>
-        if (t1.position == 0) executeCommand((commandId, command))
+        if (train.position == 0)
+          executeCommand(train, (commandId, command))
         else commandId + 1
     }
-
 }
